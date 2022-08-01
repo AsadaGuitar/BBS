9e8aa8d7f55b4a88813fb393a302c4b93fea7797
Junichi Kato <j5ik2o@gmail.com>
2022-08-01 22:46:18 +0900

    Change the pattern matching method

--- interface/src/main/scala/com/github/asadaGuitar/bbs/interfaces/adaptors/slick/SlickUsersRepositoryImpl.scala
+++ interface/src/main/scala/com/github/asadaGuitar/bbs/interfaces/adaptors/slick/SlickUsersRepositoryImpl.scala
@@ -1,6 +1,12 @@
 package com.github.asadaGuitar.bbs.interfaces.adaptors.slick
 
-import com.github.asadaGuitar.bbs.domains.models.{EmailAddress, User, UserId, UserName, UserPassword}
+import com.github.asadaGuitar.bbs.domains.models.{
+  EmailAddress,
+  User,
+  UserId,
+  UserName,
+  UserPassword
+}
 import com.github.asadaGuitar.bbs.interfaces.adaptors.slick.dao.Tables
 import com.github.asadaGuitar.bbs.repositories.UsersRepository
 import com.github.asadaGuitar.bbs.repositories.models.UserForm
@@ -9,25 +15,26 @@ import java.sql.Timestamp
 import java.util.Date
 import scala.concurrent.{ExecutionContext, Future}
 
-final class SlickUsersRepositoryImpl(implicit ec: ExecutionContext) extends SlickDbConfigProvider with UsersRepository {
+final class SlickUsersRepositoryImpl(implicit ec: ExecutionContext)
+    extends SlickDbConfigProvider
+    with UsersRepository {
   import dbConfig.profile.api._
 
-  override def save(userForm: UserForm): Future[Int] =
-    userForm match {
-      case UserForm(id, emailAddress, firstName, lastName, password) =>
-        dbConfig.db.run {
-          Tables.Users.insertOrUpdate(
-            Tables.UsersRow(
-              id = id.value,
-              firstName = firstName.value,
-              lastName = lastName.value,
-              emailAddress = emailAddress.value,
-              password = password.value,
-              createAt = new Timestamp(new Date().getTime)
-            )
-          )
-        }
+  override def save(userForm: UserForm): Future[Int] = {
+    val UserForm(id, emailAddress, firstName, lastName, password) = userForm
+    dbConfig.db.run {
+      Tables.Users.insertOrUpdate(
+        Tables.UsersRow(
+          id = id.value,
+          firstName = firstName.value,
+          lastName = lastName.value,
+          emailAddress = emailAddress.value,
+          password = password.value,
+          createAt = new Timestamp(new Date().getTime)
+        )
+      )
     }
+  }
 
   override def findById(userId: UserId): Future[Option[User]] =
     for {
@@ -38,7 +45,17 @@ final class SlickUsersRepositoryImpl(implicit ec: ExecutionContext) extends Slic
           .headOption
       }
     } yield record.map {
-      case Tables.UsersRow(id, first_name, last_name, email_address, password, is_close, create_at, modify_at, close_at) =>
+      case Tables.UsersRow(
+            id,
+            first_name,
+            last_name,
+            email_address,
+            password,
+            is_close,
+            create_at,
+            modify_at,
+            close_at
+          ) =>
         User(
           id = UserId(id),
           firstName = UserName(first_name),
